9c7a61dd225b9db655b1cb2d4da9fcc32e41441a
Catherine Murdoch <catherine@catherinemurdoch.net>
2012-11-20 18:16:49 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Heard about this being a challenge in some interviews for programming positions to weed out the people who lied on their job applications about actually being able to program. 
---
+++ fizzbuzz.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class FizzBuzz{
+
+	public static void main(String[] args){
+		int i = 1;		
+		for(;i < 100; i++){
+			if(((i%3)==0) && ((i%5)==0)){
+				System.out.println("FizzBuzz");
+			}
+			else if((i%3)==0){
+				System.out.println("Fizz");
+			}
+			else if((i%5)==0){
+				System.out.println("Buzz");
+			}
+			else{
+				System.out.println(i);
+			}	
+		}		
+		return;
+	}
+}
